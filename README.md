fbe920d1dfbd1e40c6118573db9e44bc2e3d84f0
EnanDorta <junior.goncalves01@hotmail.com>
2022-03-06 01:03:04 -0300

    desafio finalizado

---
+++ README.md
@@ -0,0 +1,39 @@
+<h1 align="center">
+  RocketShoes
+</h1>
+
+- - -
+
+![GitHub repo size](https://img.shields.io/github/repo-size/EnanDorta/desafio03-react-ignite)
+![GitHub language count](https://img.shields.io/github/languages/count/EnanDorta/desafio03-react-ignite)
+![GitHub top language](https://img.shields.io/github/languages/top/EnanDorta/desafio03-react-ignite)
+
+<h1>Preview</h1>
+<p align="center">
+  <img alt="RocketShoes" src=".github/rocketShoes.gif" width="100%">
+</p>
+
+<br>
+
+## Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- STYLED COMPONENTS
+- REACT 
+- TYPESCRIPT
+- FAKE API com JSON Server
+
+## Projeto
+
+ Para rodar esse código em sua máquina, basta usar os comandos: 
+
+ - yarn 
+ - yarn server 
+ - yarn start
+
+ Esse projeto é um desafio da Rocketseat da trilha de ReactJs, é basicamente um website para vendas de sapatos, onde escolhemos o produto desejado e depois colocamos no carrinho onde é possível alterar a quantidade e ver o valor total, o carrinho está sendo salvo usando o localStorage do navegador com isso salvamos a quantidade e valor dos produtos no carrinho. O foco desse desafio foi criar um hook para o carrinho de compras, e com isso listar a quantidade dos produtos que estão no carrinho, atualizar a quantidade e também removê-los se preciso. Os produtos são rendenrizado em tela que vem da fake API e também é calculado o valor total do carrinho.
+- - -
+
+ Template desenvolvido pela Rocketseat como desafio, e criação do hook do carrinho de compras feita por Enan Dorta.
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product} from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,28 +23,75 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
+  const prevCartRef = useRef<Product[]>();
+
+  useEffect(() => {
+    prevCartRef.current = cart;
+  })
+
+  const cartPreviousValue = prevCartRef.current ?? cart;
+
+  useEffect(() => {
+    if (cartPreviousValue !== cart) {
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+    }
+  }, [cartPreviousValue, cart]);
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.map(product => ({...product}))
+      const productsExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      const currentAmount = productsExists ? productsExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return
+      }
+      if (productsExists) {
+        productsExists.amount = amount;
+      } else {
+        const product = await api.get(`/products/${productId}`)
+
+        const newProduct = {
+          ...product.data,
+          amount: 1,
+        }
+        updatedCart.push(newProduct);
+      }
+      setCart(updatedCart)
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.map(product => ({...product}))
+
+      const productIndex = updatedCart.findIndex(product => product.id === productId)
+
+      if (productIndex >= 0) {
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+        toast.success('Produto removido do carrinho com sucesso')
+      } else {
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
@@ -53,9 +100,28 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return;
+      }
+      const stock = await api.get(`/stock/${productId}`)
+
+      const stockAmount = stock.data.amount;
+
+      if ( amount > stockAmount ) {
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+      const updatedCart = cart.map(product => ({...product}))
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      if (productExists) {
+        productExists.amount = amount;
+        setCart(updatedCart);
+      } else {
+        throw Error()
+      }
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
